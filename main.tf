f7a6f280cde8eb97e2bce2f3d9d2b543f29548be
jangshin-cho <83290851+jangshin-cho@users.noreply.github.com>
2021-05-04 15:09:16 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+  
+  backend "azurerm" {
+    resource_group_name   = "terraform01"
+    storage_account_name  = "jangshinterraform"
+    container_name        = "terraform01"
+    key                   = "terraform.tfstate"
+  }
+  
+  required_version = ">= 0.14.9"
+  
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
 resource "azurerm_resource_group" "terraform-cli" {
   
   name     = "terraform-cli"
